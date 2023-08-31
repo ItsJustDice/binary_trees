a01f0588683ecb2aa37c76a10e6b6b80b147860f
Candice <122986059+ItsJustDice@users.noreply.github.com>
2023-08-31 19:46:47 +0200

    112-array_to_bst.c

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * array_to_bst - builds a Binary Search Tree from an array
  * @array: pointer to the first element of the array to be converted
@@ -9,11 +8,11 @@
  */
 bst_t *array_to_bst(int *array, size_t size)
 {
-	unsigned int i;
+	unsigned int j; /* initalizes variable j and pointer root to NULL */
 	bst_t *root = NULL;
+	/* Iterates elements of the input array */
+	for (j = 0; j < size; j++) /* value of j is passed as 2nd arguement */
+		bst_insert(&root, array[j]);
 
-	for (i = 0; i < size; i++)
-		bst_insert(&root, array[i]);
-
-	return (root);
+	return (root); /* returns search tree containing all values */
 }
