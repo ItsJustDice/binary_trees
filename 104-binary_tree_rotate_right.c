fc63a1002348080c303f417f6eb9fe2900229fc7
Candice <122986059+ItsJustDice@users.noreply.github.com>
2023-08-31 19:12:57 +0200

    104-binary_tree_rotate_right.c

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_rotate_right - Function that  rotates right the binary tree.
  * @tree: The root node of the three
@@ -7,20 +6,21 @@
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-	binary_tree_t *pivot;
-
+	binary_tree_t *blk;
+	/* checks if input tree is NULL */
 	if (tree == NULL || tree->left == NULL)
 	{
-		return (NULL);
+		return (NULL); /* return 0 if empty or NULL */
 	}
-	pivot = tree->left;
-	tree->left = pivot->right;
-	if (pivot->right != NULL)
+	blk = tree->left; /* code proceeds with rotation if not NULL */
+	tree->left = blk->right; /* l is updated to r, if r is blk, update P */
+	if (blk->right != NULL)
 	{
-		pivot->right->parent = tree;
+		/* r child of blk is tree, P is updated to blk */
+		blk->right->parent = tree;
 	}
-	pivot->right = tree;
-	pivot->parent = tree->parent;
-	tree->parent = pivot;
-	return (pivot);
+	blk->right = tree;
+	blk->parent = tree->parent;
+	tree->parent = blk;
+	return (blk); /* returns blk as new root of binary tree */
 }
