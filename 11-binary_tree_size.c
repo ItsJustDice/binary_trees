a09934c108b0081356dcb5de24124b020e5cd536
Candice <122986059+ItsJustDice@users.noreply.github.com>
2023-08-31 17:14:48 +0200

    11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,16 +1,15 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_size - measures the size of a binary tree
  * @tree: tree to measure the size of
- *
- * Return: size of the tree
- *         0 if tree is NULL
+ * Return: 0 if tree is NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (0);
-
+		return (0); /* Return if tree is empty(NULL) */
+	/* If tree is not empty, it recursively calls itself left & right */
 	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	/* size of tree is calculated by adding sizes of l&r */
+	/* adding 1 root node, returned as the size of binary tree */
 }
