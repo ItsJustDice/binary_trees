4783ce196cae1028693d5ed59e33b8549e61b402
Candice <122986059+ItsJustDice@users.noreply.github.com>
2023-08-31 17:50:05 +0200

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,21 +1,45 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree
- * @tree: A pointer to the root node of the tree to count the number of nodes
- * Return: 0 if tree is NULL.
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: tree to count the nodes from
+ * Return: number of nodes counted
+ *         0 if tree is NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t node = 0; /* sumup variable nodes to keep count in tree */
+	if (!tree || (!tree->left && !tree->right))
+		return (0); /* Return 0 if empty, or NULL */
+	/* Subtracts num of leaf nodes calculates size of entire tree */
+	return (binary_tree_size(tree) - binary_tree_leaves(tree));
+}
 
-	if (tree)
-	{
-		/* recursively calls itself on l&r subtrees of current node */
-		/* checks if tree is empty NUULL, Returns 0 */
-		node += (tree->left || tree->right) ? 1 : 0;
-		node += binary_tree_nodes(tree->left);
-		node += binary_tree_nodes(tree->right);
-	}
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: tree to measure the size of
+ * Return: size of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0); /* return 0 if empty or NUll, or recursively l&r */
+	/*size is calculated by adding size of l&r subtrees, 1 for root node */
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
 
-	return (nodes); /* Return to value of node */
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: tree to count the leaves from
+ * Return: number of leaves
+ *         0 if tree is NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0); /* Returns 0 if empty or NULL */
+
+	if (!tree->left && !tree->right)
+		return (1); /* calls itself on l&r subtrees of current node */
+	/* returned as the size of the binary tree */
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
